c9733858c47715a7f0e933d0bd3ba6e140a1181e
seleb <sean.s.leblanc@gmail.com>
2016-04-03 00:58:08 -0400

    sort of a menu?

--- Grass Game/Source/Game/src/MY_Scene_Menu.cpp
+++ Grass Game/Source/Game/src/MY_Scene_Menu.cpp
@@ -8,12 +8,16 @@
 MY_Scene_Menu::MY_Scene_Menu(Game * _game) :
 	MY_Scene_Base(_game)
 {
-	TextLabel * t = new TextLabel(uiLayer->world, font, textShader);
-	t->setText("press enter to start");
-	uiLayer->addChild(t);
+	NodeUI * menu1 = new NodeUI(uiLayer->world);
+	uiLayer->addChild(menu1);
+	menu1->setRationalHeight(1.f, uiLayer);
+	menu1->setSquareWidth(1.f);
+	menu1->background->mesh->pushTexture2D(MY_ResourceManager::globalAssets->getTexture("MENU")->texture);
+	menu1->background->mesh->setScaleMode(GL_NEAREST);
 }
 
 void MY_Scene_Menu::update(Step * _step){
+	uiLayer->resize(0,0,64,64);
 	if(keyboard->keyJustDown(GLFW_KEY_ENTER)){
 		game->scenes["main"] = new MY_Scene_Main(game);
 		game->switchScene("main", false);
